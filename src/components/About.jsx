daf97b702974e8ba1aab06196471971c19fd35ec
Santiago Crippa <santicrippa@gmail.com>
2023-07-01 20:47:03 -0300

    just detail

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <>
+            <div className='w-full absolute -z-10 mx-auto mt-24'>
+                <div className='h-[100vh] text-center'>
+                    <p>Home</p>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <>
-            <div className='w-full absolute -z-10 mx-auto bg-emerald-500 mt-24'>
+            <div className='w-full absolute -z-10 mx-auto mt-24'>
                 <div className='h-[100vh] text-center'>
                     <p>Home</p>
                 </div>
