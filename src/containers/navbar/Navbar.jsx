000816ecbc43ccff0bc8ca0a7f88002b7aea6200
Santiago Crippa <santicrippa@gmail.com>
2023-07-07 15:43:19 -0300

    home early design concept

--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -33,49 +33,23 @@ const Navbar = () => {
       </nav>
 
       <nav className=
-        {`flex w-1/2 md:items-center sm:px-4 md:gap-3 text-light text-md bg-indigo-600 px-5 mt-2 z-10 h-screen fixed -top-2 transition-color duration-500 ease-in md:hidden
+        {`flex w-1/2 md:items-center sm:px-4 md:gap-3 text-light text-md bg-primary px-5 mt-2 z-10 h-screen fixed -top-2 transition-color duration-500 ease-in md:hidden
         ${sideMenu ? '-left-[100%]' : 'fixed left-0 shadow-lg shadow-dark'}`}>
         <div className='text-black flex flex-col uppercase'>
-          <ul className='flex flex-col mt-5 justify-start items-start font-mono font-bold text-lg mx-auto gap-5 '>
+          <ul className='flex flex-col mt-5 justify-start items-start mx-auto gap-5 '>
             <Link
-              className='flex gap-3 transition ease-in-out hover:scale-110 duration-100 text-slate-50 hover:text-blue-300' to={'/home'}>
+              className='flex gap-3 text-lg font-extrabold transition ease-in-out hover:scale-110 duration-100 text-dark hover:text-blue-300' to={'/home'}>
               <img src={home} alt="home" />Home</Link>
             <Link
-              className='flex gap-3 transition ease-in-out hover:scale-110 duration-100 text-slate-50 hover:text-blue-300' to={"/about"} >
+              className='flex gap-3 text-lg font-extrabold transition ease-in-out hover:scale-110 duration-100 text-dark hover:text-blue-300' to={"/about"} >
               <img src={about} alt="about" />About</Link>
             <Link
-              className='flex gap-3 transition ease-in-out hover:scale-110 duration-100 text-slate-50 hover:text-blue-300' to={"/works"} >
+              className='flex gap-3 text-lg font-extrabold transition ease-in-out hover:scale-110 duration-100 text-dark hover:text-blue-300' to={"/works"} >
               <img src={works} alt="works" />Works</Link>
             <Link
-              className='flex gap-3 transition ease-in-out hover:scale-110 duration-100 text-slate-50 hover:text-blue-300' to={"/contact"} >
+              className='flex gap-3 text-lg font-extrabold transition ease-in-out hover:scale-110 duration-100 text-dark hover:text-blue-300' to={"/contact"} >
               <img src={contact} alt="contact" />Contact</Link>
           </ul>
-          <div className='w-full h-screen align-bottom py-96 translate-x-7'>
-            <div className='flex gap-5 '>
-              <button>
-                <a
-                  className='flex align-bottom'
-                  href="https://github.com/sjcrippa"
-                  target='_blank'>
-                  <img
-                    className='transition ease-in-out hover:scale-110 duration-200 h-10 w-10'
-                    src={github}
-                    alt="github" />
-                </a>
-              </button>
-              <button>
-                <a
-                  href="https://www.linkedin.com/in/santiago-julian-crippa/"
-                  target='_blank'>
-                  <img
-                    className='transition ease-in-out hover:scale-110 duration-200 h-10 w-10'
-                    src={linkedin}
-                    alt="linkedin" />
-                </a>
-              </button>
-
-            </div>
-          </div>
         </div>
       </nav>
       <div className='md:hidden w-full flex justify-end fixed p-2 backdrop-blur-sm'>
