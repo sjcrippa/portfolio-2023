d59d17c33a8f39b627d446496fc3645355fceea3
Santiago Crippa <santicrippa@gmail.com>
2023-07-25 10:28:54 -0300

    responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,22 +9,22 @@ const Home = () => {
   return (
     <>
       <main className='home absolute w-full -z-10 mt-16'>
-        <h1 className='mt-5 text-light bg-gradient-to-r from-primary shadow-lg  shadow-primary to-secondary font-extrabold text-[3rem] w-80 mx-auto rounded text-center -mb-16'>
+        <h1 className='mt-5 text-light bg-gradient-to-r from-primary shadow-lg  shadow-primary to-secondary font-extrabold text-[3rem] w-80 mx-auto rounded text-center -mb-16 animate-fade-up animate-duration-500'>
           S.J. CRIPPA
         </h1>
         <div className="mx-auto container grid grid-cols-1 md:grid-cols-3">
 
-          <section className='mt-40 col animate-fade-right animate-once animate-duration-[600ms] animate-ease-linear animate-normal animate-fill-both'>
+          <section className='mt-16 md:mt-40 col animate-fade-right animate-once animate-duration-[600ms] animate-ease-linear animate-normal animate-fill-both'>
             <h2 className='text-light mt-12 italic text-2xl w-full font-bold tracking-wider text-center'>Front End Developer</h2>
           </section>
 
-          <section className='mt-24 animate-fade-right animate-once animate-duration-[600ms] animate-ease-linear animate-normal animate-fill-both'>
+          <section className='mt-16 md:mt-24 animate-fade-right animate-once animate-duration-[600ms] animate-ease-linear animate-normal animate-fill-both'>
             <div className='text-primary text-center'>
-              <img className='mt-16 px-2 rounded-full' src={profile} alt="profile-image" />
+              <img className='w-44 md:w-auto md:mt-16 px-2 rounded-full mx-auto' src={profile} alt="profile-image" />
             </div>
           </section>
 
-          <section className='mt-28 h-screen w-full animate-fade-right animate-once animate-duration-[600ms] animate-ease-linear animate-normal animate-fill-both'>
+          <section className='md:mt-28 h-screen w-full animate-fade-right animate-once animate-duration-[600ms] animate-ease-linear animate-normal animate-fill-both'>
             <div className='mt-16 text-center'>
               <p className='mx-auto italic text-light font-thin backdrop-blur-md'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, tempore.</p>
               <LetsTalk />
--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -12,22 +12,24 @@ const Navbar = () => {
   return (
     <>
       <nav
-        className='fixed mx-auto drop-shadow-md hover:drop-shadow-xl flex w-full bg-dark items-center z-10 shadow shadow-black '>
-        <Link to={"/home"}>
-          <img
-            className='hidden md:flex mt-1 w-16 h-16 rounded-lg cursor-default bg-transparent ml-5 hover:scale-105 duration-150'
-            src={logosjc}
-            alt="logosjc" />
-        </Link>
-        <div>
-          <button
-            className='flex md:hidden ml-4 mt-2'
-            onClick={() => setSideMenu(!sideMenu)}
-          >
-            {!sideMenu ? <img className='mx-52 sm:mx-[23rem] ease-in-out duration-500 w-10 h-14' src={arrowLeft} alt="arrow chevron left" /> : <img className='w-10 h-14' src={menu} alt="menu" />}
-          </button>
+        className='fixed mx-auto drop-shadow-md hover:drop-shadow-xl flex w-full bg-dark items-center z-10 shadow shadow-black justify-between'>
+        <div className='md:flex md:items-center md:px-4'>
+          <Link to={"/home"}>
+            <img
+              className='hidden md:flex mt-1 w-16 h-16 rounded-lg cursor-default bg-transparent ml-5 hover:scale-105 duration-150'
+              src={logosjc}
+              alt="logosjc" />
+          </Link>
+          <div>
+            <button
+              className='flex md:hidden ml-4 mt-2'
+              onClick={() => setSideMenu(!sideMenu)}
+            >
+              {!sideMenu ? <img className='mx-52 sm:mx-[23rem] ease-in-out duration-500 w-10 h-14' src={arrowLeft} alt="arrow chevron left" /> : <img className='w-10 h-14' src={menu} alt="menu" />}
+            </button>
+          </div>
+          <Items />
         </div>
-        <Items />
         <div className='flex justify-end mr-12'>
           <Icons />
         </div>
@@ -36,7 +38,7 @@ const Navbar = () => {
       <nav className=
         {`flex w-1/2 md:items-center sm:px-4 md:gap-3 text-light text-md bg-primary px-5 mt-2 z-10 h-screen fixed -top-2 transition-color duration-500 ease-in md:hidden
         ${sideMenu ? '-left-[100%]' : 'fixed left-0 shadow-lg shadow-dark'}`}>
-        <div className='text-black flex flex-col uppercase'>
+        <div className='text-light flex flex-col uppercase'>
           <ul className='flex flex-col mt-5 justify-start items-start mx-auto gap-5 '>
             <Link
               className='flex gap-3 text-lg font-extrabold transition ease-in-out hover:scale-110 duration-100' to={'/home'}>
